327d23d7938e20762d230780e632cda292ef2e32
ruszkipista <oi5672@gmail.com>
2021-10-26 10:55:46 +0100

    add more content to Array

--- 10_arrays.java
+++ 10_arrays.java
@@ -125,3 +125,28 @@ class arrays {
         }
     }
 }
+
+// Arrays Methods
+// ===============
+// public static int binarySearch(Object[] a, Object key)
+ 
+// Searches the specified array of Object ( Byte, Int , double, etc.) for the specified value
+// using the binary search algorithm. The array must be sorted prior to making this call. 
+// This returns index of the search key, if it is contained in the list;
+// otherwise, it returns ( – (insertion point + 1)).
+ 
+// public static boolean equals(long[] a, long[] a2)
+// Returns true if the two specified arrays of longs are equal to one another. 
+// Two arrays are considered equal if both arrays contain the same number of elements,
+// and all corresponding pairs of elements in the two arrays are equal. This returns true
+// if the two arrays are equal. Same method could be used by all other primitive data types
+// (Byte, short, Int, etc.)
+
+// public static void fill(int[] a, int val)
+// Assigns the specified int value to each element of the specified array of ints.
+// The same method could be used by all other primitive data types (Byte, short, Int, etc.)
+
+// public static void sort(Object[] a)
+// Sorts the specified array of objects into an ascending order, according to the natural
+// ordering of its elements. The same method could be used by all other primitive data types
+// ( Byte, short, Int, etc.)
