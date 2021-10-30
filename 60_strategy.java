4c21fc2d9c41a621bd301e5a274ad6e84b452f78
ruszkipista <oi5672@gmail.com>
2021-10-30 22:26:17 +0100

    refactor Strategy pattern

--- 60_strategy.java
+++ 60_strategy.java
@@ -5,21 +5,20 @@ This type of design pattern comes under behavior pattern.
 In Strategy pattern, we create objects which represent various strategies and a client object
 whose behavior varies as per its strategy object.
 The strategy object changes the executing algorithm of the client object.
+https://youtu.be/v9ejT8FO-7I
 */      
     public static void main(String[] args) {
-        IStrategy strategyA = new StrategyA();
-        IStrategy strategyB = new StrategyB();
         // construct a client with strategy A
-        Client client1 = new Client("X", strategyA);
+        Client client1 = new Client("X", new StrategyA());
         client1.demonstrate();
         // construct a client with Strategy B
-        Client client2 = new Client("Y", strategyB);
+        Client client2 = new Client("Y", new StrategyB());
         client2.demonstrate();
     }
 
     static class Client{
-        String name;
-        IStrategy algorithm;
+        private String name;
+        private IStrategy algorithm;
 
         public Client(String name, IStrategy algorithm){
             this.name = name;
@@ -38,12 +37,14 @@ The strategy object changes the executing algorithm of the client object.
     }
 
     static class StrategyA implements IStrategy{
+        @Override
         public String execute(){
             return "I do A";
         }
     }
 
     static class StrategyB implements IStrategy{
+        @Override
         public String execute(){
             return "I do B";
         }
