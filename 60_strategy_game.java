6a90582c1cdd018da54b028e88bf0c76257c6a5b
István Orosz <oi5672@gmail.com>
2021-11-02 15:42:20 +0000

    fix strategy game

--- 60_strategy_game.java
+++ 60_strategy_game.java
@@ -9,6 +9,12 @@ class CharacterSimU {
         // construct a Knight
         Character knight = new Character("Knight", new Sword());
         knight.describe();
+        // construct a Queen
+        Character queen = new Character("Queen", new HairPin());
+        queen.describe();
+        // construct a Troll
+        Character troll = new Character("Troll", new Axe());
+        troll.describe();
         // replace Weapon behaviour
         king.setWeaponBehaviour(new Sword());
         king.describe();
@@ -26,32 +32,48 @@ class CharacterSimU {
         public void setWeaponBehaviour(WeaponBehaviour newWeaponBehaviour){
             this.WeaponBehaviour = newWeaponBehaviour;
         }
-        public void fight(){
-            this.WeaponBehaviour.useWeapon();
+        public String fight(){
+            return this.WeaponBehaviour.useWeapon();
         }
 
         public void describe(){
-            System.out.print("I am the "+this.name+" and ");
-            this.fight();
-            System.out.println();
+            System.out.println("I am the "+this.name+" and "+this.fight());
         }
     }
 
     // WEAPON
     interface WeaponBehaviour{
-        public void useWeapon();
+        public String useWeapon();
     }
 
     static class Knife implements WeaponBehaviour{
         @Override
-        public void useWeapon(){
-            System.out.print("I can stab with knife");
+        public String useWeapon(){
+            return "I can cut with my knife";
         }
     }
     static class Sword implements WeaponBehaviour{
         @Override
-        public void useWeapon(){
-            System.out.print("I can cut with sword");
+        public String useWeapon(){
+            return "I can swing my sword";
+        }
+    }
+    static class Axe implements WeaponBehaviour{
+        @Override
+        public String useWeapon(){
+            return "I can chop with my axe";
+        }
+    }
+    static class BowAndArrow implements WeaponBehaviour{
+        @Override
+        public String useWeapon(){
+            return "I can shoot an arrow with my bow";
+        }
+    }
+    static class HairPin implements WeaponBehaviour{
+        @Override
+        public String useWeapon(){
+            return "I can stab with my hair pin";
         }
     }
 }
