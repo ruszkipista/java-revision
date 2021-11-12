8b9de845e9a7792d2e53696397ee80d174becfe3
ruszkipista <oi5672@gmail.com>
2021-11-12 19:22:15 +0000

    refactor Singleton pattern

--- 50_singleton.java
+++ 50_singleton.java
@@ -10,19 +10,31 @@ access its only object without need to instantiate the object of the class.
         //Compile Time Error: The constructor SingleObject() is not visible
         //  SingleObject object = new SingleObject();
 
-        SingleObject so1 = SingleObject.instance;
-        SingleObject so2 = SingleObject.instance;
+        SingleObjectDynamicallyInitialized so1 = SingleObjectDynamicallyInitialized.getInstance();
+        SingleObjectDynamicallyInitialized so2 = SingleObjectDynamicallyInitialized.getInstance();
         System.out.println("The two objects are the same: " + (so1==so2) );
     }
 
-    static class SingleObject{
-        public static SingleObject instance =  new SingleObject();
+    static class SingleObjectStaticallyInitialized{
+        public static SingleObjectStaticallyInitialized instance =  new SingleObjectStaticallyInitialized();
         // other class attributes
 
-        private SingleObject(){
+        private SingleObjectStaticallyInitialized(){
             // .... stuff for one-off initialization
         }
         // other methods
         // ...
+    }
+
+    // better
+    static class SingleObjectDynamicallyInitialized{
+        private static SingleObjectDynamicallyInitialized instance;
+
+        private SingleObjectDynamicallyInitialized(){}
+
+        public static SingleObjectDynamicallyInitialized getInstance(){
+            if (instance == null) instance = new SingleObjectDynamicallyInitialized();
+            return instance;
+        }
     }
 }
