dee887f9e9a3b89e091e551a7a56c6ac1fd0ce6f
ruszkipista <oi5672@gmail.com>
2021-11-13 22:23:27 +0000

    add MonoState Singleton pattern

---
+++ 50_singleton monostate.java
@@ -0,0 +1,30 @@
+import static org.junit.Assert.assertEquals;
+
+class Singleton_Pattern_Monostate {
+/*
+This version of Singleton is virtual, because it relies on static class variables
+whis are shared between all instances of the same class.
+Although the instances are different, they behave like having only one instance for the same class.
+There can be only static methods, mutating static class variables.
+*/
+    public static void main(String[] args) {
+
+        MonoState so1 = new MonoState();
+        MonoState so2 = new MonoState();
+        assertEquals(105, so1.classVar);
+        assertEquals(105, so2.classVar);
+    }
+
+    // Dynamic initialization
+    static class MonoState{
+        public static int classVar;
+        private static boolean isInitialized = false;
+
+        public MonoState(){
+            if (!isInitialized){
+                isInitialized = true;
+                classVar = 105;
+            }
+        }
+    }
+}
